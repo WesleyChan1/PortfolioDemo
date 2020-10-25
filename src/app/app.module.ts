d54e556dedbc1f8511234ade9c5e1a2755b1d5b4
F2WesleyC <wesley.chan@futuresquared.com.au>
2020-10-25 17:22:08 +1100

    before bootstrap install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-
+// Angular Material
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
 
   ],
   providers: [],
